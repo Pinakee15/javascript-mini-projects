37cd5d8626f39ce7e4ea67c76aaaa0b44b2647ba
pinakee15 <iampinakee2098@gmail.com>
2020-11-16 19:37:02 +0530

    made changes in the app.js file

--- budgetify/app.js
+++ budgetify/app.js
@@ -24,8 +24,35 @@ var budgetController = (
 				inc : []
 			}
 		}
+
 		var showItem = function(){
 			console.log(data)
+		}
+
+		var updateExpIncAndbudget = function(type , val){
+			data.totals[type] += val;
+			if (type === 'exp'){
+				totalBudget -= val;
+			}
+			else if (type === 'inc'){
+				totalBudget += val;
+			}
+			return {
+				exp : data.totals.exp ,
+				inc : data.totals.inc ,
+				totalBudget : totalBudget
+			}
+		}
+
+		var deleteItemFromBudget = function(ID , type){
+			var arr = data.allItems[type].map((curr)=>{
+				return curr.id;
+			});
+			index = arr.indexOf(ID);
+
+			if(index !== -1){
+				arr.splice
+			}
 		}
 
 		return {
@@ -47,10 +74,9 @@ var budgetController = (
 				data.allItems[type].push(newItem)
 				return newItem;
 			},
-			showItem : showItem ,
-			updateTop : function(val){
-				totalBudget += val;
-			}
+			updateExpIncAndbudget : updateExpIncAndbudget ,
+			showItem : showItem  ,
+			deleteItemFromBudget : deleteItem 
 		}
 	}
 )()
@@ -65,14 +91,18 @@ var UIController = (
 			addType : ".add__type" ,
 			addButton : ".add__btn",
 			incomeList :".income__list" ,
-			expenseList : ".expenses__list"
+			expenseList : ".expenses__list",
+			budgetValue : ".budget__value" ,
+			budgetIncome :  ".budget__income--value" ,
+			budgetExpense : ".budget__expenses--value" , 
+			container : '.container'
 		}
 		return {
 			getInput : function(){
 				return {
 					type : document.querySelector(DOMStrings.addType).value ,
-					value : document.querySelector(DOMStrings.addValue).value ,
-					description : document.querySelector(DOMStrings.addDescription).value 
+					value : parseFloat(document.querySelector(DOMStrings.addValue).value) ,
+					description : document.querySelector(DOMStrings.addDescription).value
 				}
 			},
 			getDOMStrings : function(){
@@ -110,6 +140,23 @@ var UIController = (
 				// Insert the HTML into the DOM
 				document.querySelector(DOMStrings.expenseList).insertAdjacentHTML('beforeend' , html)	
 				}
+			} ,
+			clearFields : function(){
+				list = document.querySelectorAll(DOMStrings.addDescription + ", " + DOMStrings.addValue)
+				console.log(list);
+				arrayList = Array.prototype.slice.call(list) ;
+				console.log(arrayList) ;
+				arrayList.forEach(function(ele){
+					ele.value = "";
+				});
+
+				arrayList[0].focus();
+			} ,
+			updateBudgetAndExpIncUI : function(budget , exp , inc){
+				document.querySelector(DOMStrings.budgetValue).textContent = budget ;
+				document.querySelector(DOMStrings.budgetIncome).textContent = inc ;
+				//document.querySelector(".budget__income--value").textContent = inc ; 
+				document.querySelector(DOMStrings.budgetExpense).textContent = exp ;
 			}
 		}	
 })()
@@ -125,26 +172,50 @@ var appController = (
 			addButton.addEventListener('click' , cntrlAddItem)
 			document.addEventListener('keypress' , function(event){
 				if(event.keyCode === 13 || event.which === 13){
-					cntrlAddItem()
+					cntrlAddItem();
 				}
-			})
-		}
+			});
+			document.querySelector(DOM.container).addEventListener('click' , deleteItem) ;
+		} 
 
 		var cntrlAddItem = function(){
 
 			// Get the input feilds from the UI
 			var inputs = UICntl.getInput() ;
+			console.log("This is the input...")
 			console.log(inputs)
 
-			// Add the item to the budget controller
-			newItem = budgetCntl.addItem(inputs.type , inputs.description , inputs.value)
-			budgetCntl.showItem()
-			
-			// Add the items to the UI
-			UICntl.addItemList(inputs.type , newItem)
+			if (inputs.description !== "" && inputs.value > 0 && !isNaN(inputs.value)){
 
-			// Update the Total Budget and the Income and expense on budget Controllers
-			budgetCntl.updateTop(newItem.value)
+				// Add the item to the budget controller
+				newItem = budgetCntl.addItem(inputs.type , inputs.description , inputs.value)           
+				budgetCntl.showItem()
+				
+				// Add the items to the UI
+				UICntl.addItemList(inputs.type , newItem)
+
+				// Clear the feilds 
+				UICntl.clearFields();
+
+
+				// Update the Total Budget and the Income and expense on budget Controllers
+				var output = budgetCntl.updateExpIncAndbudget(inputs.type , parseFloat(inputs.value))
+				budgetCntl.showItem();
+				console.log("this is the updated output ...")
+				console.log(output)
+
+				// Add these values to the UI
+				UICntl.updateBudgetAndExpIncUI(output.totalBudget , output.exp , output.inc);
+			}
+		}
+
+		var deleteItem = function(event){
+			var splitID , type , ID;
+			splitID = event.target.parentNode.parentNode.parentNode.parentNode.id.split("-") ;
+			type = splitID[0];
+			ID = splitID[1];
+
+			// Delete the item from the datastructure ....
 		}
 
 		return {
